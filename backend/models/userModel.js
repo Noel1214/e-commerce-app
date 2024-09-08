1957440f4c59bcaea8d3ab257fb4d9f10c1a25ab
Noel Sebu <noelsebu555@gmail.com>
2024-09-08 11:47:43 +0530

    created an instance method on userSchema for creating auth token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,34 +1,52 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     userName: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     phone: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
-}, {timestamps: true});
+  },
+  { timestamps: true }
+);
 
+userSchema.methods.generateAuthToken = function () {
+  try {
+    return jwt.sign(
+      {
+        userId: this._id.toString(),
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
