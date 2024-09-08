baee8d89dab7b6f98aad54056a1698a00bdd1571
Noel Sebu <noelsebu555@gmail.com>
2024-09-08 12:24:24 +0530

    imported userLogout controller and setting it for '/logout' sub route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { createUser, userLogin } = require('../controllers/userController');
+const { createUser, userLogin, userLogout } = require('../controllers/userController');
 
 router.route('/register').post(createUser);
 router.route('/login').post(userLogin);
+router.route('/logout').post(userLogout);
 
 module.exports = router;
