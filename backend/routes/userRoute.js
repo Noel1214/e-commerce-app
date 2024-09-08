2a3a7a5aa6ffb52535f989a2b937200674da7947
Noel Sebu <noelsebu555@gmail.com>
2024-09-08 11:57:04 +0530

    importing userLogin controller and setting it for new '/login' sub route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { createUser } = require('../controllers/userController');
+const { createUser, userLogin } = require('../controllers/userController');
 
 router.route('/register').post(createUser);
-
-
+router.route('/login').post(userLogin);
 
 module.exports = router;
