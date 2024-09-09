b607098f8b6eaeb64b7c50c9566f59e43c0340ad
Noel Sebu <noelsebu555@gmail.com>
2024-09-09 18:43:50 +0530

    importing and setting up editUserDetails controller to '/update/:id' sub route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { createUser, userLogin, userLogout } = require('../controllers/userController');
+const {
+  createUser,
+  userLogin,
+  userLogout,
+  editUserDetails,
+} = require("../controllers/userController");
 
-router.route('/register').post(createUser);
-router.route('/login').post(userLogin);
-router.route('/logout').post(userLogout);
+router.route("/register").post(createUser);
+router.route("/login").post(userLogin);
+router.route("/logout").post(userLogout);
+router.route("/update/:id").put(editUserDetails);
 
 module.exports = router;
