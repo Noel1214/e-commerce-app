cce4969aba2220aab4500be5bec08487128d4bdb
Noel Sebu <noelsebu555@gmail.com>
2024-09-23 10:21:54 +0530

    added the slide bar to app by importing it and adding to pages file

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SlideBar from '@/components/SlideBar'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <SlideBar />
+    </div>
   )
 }
 
