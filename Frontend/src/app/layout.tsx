00e297436f8f5b437b21034287ac8aca16d3e28c
Noel Sebu <noelsebu555@gmail.com>
2024-10-05 11:24:10 +0530

    injected slidebar into layout as out of flow as a seperate div

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SlideBar from "@/components/SlideBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="relative flex">
+          <div className="absolute">
+            <SlideBar />
+          </div>
+          <div className="ml-[5rem] p-2">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
