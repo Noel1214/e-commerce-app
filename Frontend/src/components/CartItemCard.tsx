6630d05a407c63657e8a53d67cdd42fb21bd2088
Noel Sebu <noelsebu555@gmail.com>
2024-11-06 21:53:24 +0530

    default CartItemCard UI component will be hidden for small screen SmallCartItemCard UI component will be displayed for small screens unit of width changed from rem to vw due to breaking when going below 1000px screen width, imported SmallCartItemCard

--- Frontend/src/components/CartItemCard.tsx
+++ Frontend/src/components/CartItemCard.tsx
@@ -1,44 +1,51 @@
 import React from 'react'
+import SmallCartItemCard from './SmallCartItemCard'
 
-const CartItemCard = ({image}) => {
+const CartItemCard = ({ image }) => {
     return (
-        <div className='bg-zinc-900 w-[68vw] rounded-lg overflow-hidden'>
-            <div className='flex text'>
-                <div className='shrink-0 border-r-2 border-zinc-700'>
-                    <img className='h-[13rem] w-[14rem] object-cover shrink-0' src={`${image}`} alt="" />
-                </div>
-                <div className='flex justify-between mx-8 mt-10 w-[40rem]'>
-                    <div className='w-[10rem] overflow-hidden flex flex-col items-center gap-3 text-center'>
-                        <div>
-                            <h1 className='text-xl'>Nike</h1>
-                        </div>
-                        <div>
-                            <h1 className='text-xs'>Product id:</h1>
-                            <h1 className='text-sm'>3948238</h1>
-                        </div>
+        <>
+        {/* For Large Screens */}
+            <div className='hidden sm:flex bg-zinc-900 w-[68vw] rounded-lg overflow-hidden'>
+                <div className='flex'>
+                    <div className='border-r-2 border-zinc-700'>
+                        <img className='h-[13rem] w-[16vw] object-cover shrink-0' src={`${image}`} alt="" />
                     </div>
-                    <div className='flex flex-col items-center gap-2'>
-                        <h1>Quantity</h1>
-                        <div className='flex flex-col items-center'>
-                            <button>+</button>
-                            <h1>1</h1>
-                            <button>-</button>
+                    <div className='flex justify-between gap-10 mx-3 mt-10 w-[45vw]'>
+                        <div className='w-[10rem] overflow-hidden flex flex-col items-center gap-3 text-center'>
+                            <div>
+                                <h1 className='text-xl'>Nike</h1>
+                            </div>
+                            <div>
+                                <h1 className='text-xs'>Product id:</h1>
+                                <h1 className='text-sm'>3948238</h1>
+                            </div>
                         </div>
-                    </div>
-                    <div className='flex flex-col gap-4 items-center'>
-                        <h1 className=''>Total Price</h1>
-                        <div className='flex gap-1'>
-                            <h1>$</h1>
-                            <h1>8274</h1>
+                        <div className='flex flex-col items-center gap-2'>
+                            <h1>Quantity</h1>
+                            <div className='flex flex-col items-center'>
+                                <button>+</button>
+                                <h1>1</h1>
+                                <button>-</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className='flex flex-col gap-5'>
-                        <button className='bg-teal-500 p-2 rounded-lg text-center'>Buy Now</button>
-                        <button className='bg-red-500 p-2 rounded-lg text-center'>Remove</button>
+                        <div className='flex flex-col gap-4 items-center'>
+                            <h1 className=''>Total Price</h1>
+                            <div className='flex gap-1'>
+                                <h1>$</h1>
+                                <h1>8274</h1>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-5'>
+                            <button className='bg-teal-500 p-2 rounded-lg text-center'>Buy Now</button>
+                            <button className='bg-red-500 p-2 rounded-lg text-center'>Remove</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div>
+
+            </div>
+        </>
     )
 }
 
