eaf9aa22d0c278aa209574079568c54210c00c0c
Noel Sebu <noelsebu555@gmail.com>
2024-11-11 23:27:56 +0530

    main file added componets randomly to visulaize positions

--- Frontend/src/app/(main)/page.tsx
+++ Frontend/src/app/(main)/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react'
+import CartItemCard from '@/components/CartItemCard'
 
 const page = () => {
-  return <div className="">I am page in main</div>;
-};
+  return (
+    <>
+      <div>I am main</div>
+      <div className='flex flex-col gap-7'>
+      </div>
+    </>
+  )
+}
 
-export default page;
+export default page
